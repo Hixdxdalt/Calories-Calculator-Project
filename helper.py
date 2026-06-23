227bb659e9b1c0b2e7d26acc12452f2daf388350
Hixdxdalt <tongploytubtim@gmail.com>
2026-06-23 21:50:59 +0700

    Finished implementing merging function

--- helper.py
+++ helper.py
@@ -1,39 +1,41 @@
 import pandas as pd
 import numpy as np
+def merger(pathCal,pathMass):
+    df1 = pd.read_csv(pathCal)
+    df2 = pd.read_csv(pathMass)
 
-df1 = pd.read_csv("Raw data/Calories.csv")
-df2 = pd.read_csv("Raw data/Weight.csv")
+    # Convert dates
+    df1["Date"] = pd.to_datetime(df1["Date"])
+    df2["Date"] = pd.to_datetime(df2["Date"])
 
-# Convert dates
-df1["Date"] = pd.to_datetime(df1["Date"])
-df2["Date"] = pd.to_datetime(df2["Date"])
+    # Daily calories
+    daily_calories = (
+        df1.groupby("Date", as_index=False)["Calories"]
+        .sum()
+    )
 
-# Daily calories
-daily_calories = (
-    df1.groupby("Date", as_index=False)["Calories"]
-       .sum()
-)
+    daily_calories["Calories"] = np.ceil(
+        daily_calories["Calories"]
+    ).astype(int)
 
-daily_calories["Calories"] = np.ceil(
-    daily_calories["Calories"]
-).astype(int)
+    # Daily weight
+    daily_weight = (
+        df2.groupby("Date", as_index=False)["Weight"]
+        .mean()
+    )
 
-# Daily weight
-daily_weight = (
-    df2.groupby("Date", as_index=False)["Weight"]
-       .mean()
-)
+    # Merge using calories as the date list
+    merged_df = pd.merge(
+        daily_calories,
+        daily_weight,
+        on="Date",
+        how="left"
+    )
 
-# Merge using calories as the date list
-merged_df = pd.merge(
-    daily_calories,
-    daily_weight,
-    on="Date",
-    how="left"
-)
+    # Forward fill weights
+    merged_df["Weight"] = merged_df["Weight"].ffill()
 
-# Forward fill weights
-merged_df["Weight"] = merged_df["Weight"].ffill()
+    # Save
+    #merged_df.to_csv("Finale.csv", index=False)
 
-# Save
-merged_df.to_csv("Merged_Calories_Weight.csv", index=False)
+    return merged_df
